12409372a5d8e102a83ada24c97dbbdeb56eb6f0
peteballs <55283832+peteballs@users.noreply.github.com>
2019-10-14 21:32:43 -0500

    Add files via upload

---
+++ numberstowords.py
@@ -0,0 +1,20 @@
+def num2words(num):
+	under_20 = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen']
+	tens = ['Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety']
+	above_100 = {100: 'Hundred',1000:'Thousand', 1000000:'Million', 1000000000:'Billion'}
+ 
+	if num < 20:
+		 return under_20[num]
+	
+	if num < 100:
+		return tens[(int)(num/10)-2] + ('' if num%10==0 else ' ' + under_20[num%10])
+ 
+	pivot = max([key for key in above_100.keys() if key <= num])
+ 
+	return num2words((int)(num/pivot)) + ' ' + above_100[pivot] + ('' if num%pivot==0 else ' ' + num2words(num%pivot))
+
+num = int(input("Enter a number......."))
+print ("Your selection is.......")
+print(num2words(num))
+
+
